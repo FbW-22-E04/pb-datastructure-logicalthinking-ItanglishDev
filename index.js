acbd701e103ec97007c2c6a1d9657eb275f545ef
chris <chrishethertonweb@gmail.com>
2022-08-23 14:42:15 +0200

    ex 2 done

--- index.js
+++ index.js
@@ -6,3 +6,9 @@
 const array = [4, 2, 5, 3, 9, 7, 1, 8, 2, 6];
 
 console.log(array.sort());
+
+// EX 2
+
+const arrayOfNames = ["Fran", "Leon", "Ale", "Jenna", "Bleda", "Hadi"];
+
+console.log(arrayOfNames.sort());
